4366222630ab130ad36b811f1f566925b0942e5b
kathan3009 <kthndesai@gmail.com>
2020-07-14 18:10:52 +0530

    basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,23 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  firebase:
+    {
+      apiKey: "AIzaSyBlHuzACH7LdiMb46Ph5yvb2r-lJEEPo_w",
+      authDomain: "vaani-7d50e.firebaseapp.com",
+      databaseURL: "https://vaani-7d50e.firebaseio.com/",
+      projectId: "vaani-7d50e",
+      storageBucket: "vaani-7d50e.appspot.com",
+      messagingSenderId: "319038459746",
+      appId: "1:319038459746:web:86b84fb6ddd765c617663b",
+      measurementId: "G-RXQF4QBWDL"
+    }
+};
